f49115d7fcfc42085d8486fda4b2c63848d5604b
unknown <lazarlemic2@gmail.com>
2023-03-03 14:54:59 +0100

    simplified code a lot, tested some networks and their output

--- Train_BGFG_BCE_with_weightsUnet3.py
+++ Train_BGFG_BCE_with_weightsUnet3.py
@@ -6,7 +6,8 @@ from metrics_utils import*
 from configUnet3 import config_func_unet3
 import argparse
 import logging
-
+import matplotlib.pyplot as plt
+from PIL import Image
 
 def end_of_epoch_print(epoch,all_validation_losses):
     ispis = "{{metricName: loss, metricValue: {:.4f}}}\n".format(all_validation_losses[epoch])
@@ -15,202 +16,150 @@ def end_of_epoch_print(epoch,all_validation_losses):
 
 def main(putanja_train, putanja_val, putanja_test, p_index,lr,lambda_p,step, num_epochs, loss_type,Batch_size, cfg):
 
+    #Setting the variables
     lr = lr
     lambda_parametri = lambda_p
     stepovi = step
     epochs = num_epochs
     batch_size = Batch_size
 
-    ####################
-    ### data loading ###
-    ####################
-
+    #Loading datasets
     train_loader, valid_loader = data_loading(putanja_train, putanja_val, cfg.binary, p_index, cfg.net_type, batch_size)
-
-    ############################
-    ### model initialization ###
-    ############################
-
+    test_loader = AgroVisionDataLoader(cfg.img_size, putanja_test, cfg.img_data_format, p_index,batch_size, 
+                                       cfg.device, cfg.zscore, cfg.binary, cfg.dataset)
+    #Model and loss init
     segmentation_net = model_init(cfg.num_channels, cfg.num_channels_lab, cfg.img_h, cfg.img_w, cfg.zscore,
                                 cfg.net_type, cfg.device, cfg.server, cfg.GPU_list)
     segmentation_net = torch.nn.DataParallel(segmentation_net, device_ids=[0]).to(cfg.device)
-    # print(summary(segmentation_net))
-    ############################
-    ### model initialization ###
-    ############################
-
 
     optimizer, scheduler = optimizer_init(segmentation_net, lr, cfg.weight_decay, cfg.scheduler_lr, lambda_parametri, cfg.optimizer_patience)
-
-    ############################
-    ### Loss initialization ###
-    ############################
-
     criterion = loss_init(cfg.use_weights, loss_type, cfg.dataset, cfg.num_channels_lab, cfg.device, cfg.use_mask)
 
-    # Brojanje Iteracija
+    # Counting Iterations
     epoch_list = np.zeros([epochs])
     all_train_losses = np.zeros([epochs])
     all_validation_losses = np.zeros([epochs])
 
     for epoch in range(epochs):
 
+        #Training
         train_part = "Train"
         segmentation_net.train(mode=True)
-
-        index_start = 0
-
-        batch_iou = torch.zeros(size=(len(train_loader.dataset.img_names), cfg.num_channels_lab*2), device= cfg.device, dtype=torch.float32)
-
-        loss_type = 'bce'
-
-        if loss_type == 'bce':
-            batch_iou_bg = torch.zeros(size=(len(train_loader.dataset.img_names),2), device = cfg.device, dtype=torch.float32)
-
 
         for input_var, target_var, batch_names_train in train_loader:
 
             set_zero_grad(segmentation_net)
 
             model_output = segmentation_net.forward(input_var)
-            # print("############### TRAIN #####################")
-            # print(torch.unique(input_var))
-            # print(model_output)
-            # print(target_var)
-            # print(model_output.shape)
-            # print(target_var.shape)
-            # print(len(model_output))
-            # print(len(target_var))
-            print("################ TRAIN #####################")
+            print("Training: ", end="")
             loss = loss_calc(loss_type, criterion, model_output, target_var, cfg.num_channels_lab, cfg.use_mask)
             loss.backward()
             print(loss)
             optimizer.step()  # mnozi sa grad i menja weightove
-
             cfg.train_losses.append(loss.data)
-
-            ######## update!!!!
-
-            index_end = index_start + len(batch_names_train)
-            if loss_type == 'bce':
-                batch_iou[index_start:index_end, :], batch_iou_bg[index_start:index_end] = calc_metrics_pix(model_output, target_var,
-                 cfg.num_channels_lab, cfg.device, cfg.use_mask, loss_type)
-            elif loss_type == 'ce':
-                batch_iou[index_start:index_end, :]= calc_metrics_pix(model_output, target_var, 
-                                                        cfg.mask_train, cfg.num_channels_lab, cfg.device, cfg.use_mask,loss_type)
-            else:
-                print("Error: unimplemented loss type")
-                sys.exit(0)
-            index_start += len(batch_names_train)
-
             cfg.count_train += 1
 
-        if loss_type == 'bce':
-            final_metric_calculation(loss_type = loss_type,epoch=epoch,num_channels_lab = cfg.num_channels_lab,classes_labels = cfg.classes_labels,
-            batch_iou_bg=batch_iou_bg,batch_iou=batch_iou,train_part= train_part)
-        elif loss_type == 'ce':
-            final_metric_calculation(loss_type = loss_type,epoch=epoch,num_channels_lab=cfg.num_channels_lab,classes_labels = cfg.classes_labels,
-            batch_iou=batch_iou,train_part= train_part)
-        else:
-            print("Error: Unimplemented loss type!")
-            sys.exit(0)
-
         all_train_losses[epoch] = (torch.mean(torch.tensor(cfg.train_losses,dtype = torch.float32)))
-        input_var2 = input_var
-
         if epoch !=0 and (epoch % stepovi)==0 :
             print("epoha: " + str(epoch) +" , uradjen step!")
 
         if cfg.server:
             torch.cuda.empty_cache()
+
+
+        #Validation
         train_part = "Valid"
-
-        # model_output = segmentation_net.forward(input_var2)
-        # print("DEBUG: ")
-        # print()
-        # print()
-        # print(model_output)
-        # print()
-        # print()
-
-
         segmentation_net.eval()
+        # index_start = 0
 
-        # model_output = segmentation_net.forward(input_var2)
-        # print("DEBUG: ")
-        # print()
-        # print()
-        # print(model_output)
-        # print()
-        # print()
-
-        index_start = 0
-
-        batch_iou = torch.zeros(size=(len(valid_loader.dataset.img_names), cfg.num_channels_lab*2),device=cfg.device,dtype=torch.float32)
-        if loss_type == 'bce':
-            batch_iou_bg = torch.zeros(size=(len(valid_loader.dataset.img_names),2),device=cfg.device,dtype=torch.float32)
+        # batch_iou = torch.zeros(size=(len(valid_loader.dataset.img_names), cfg.num_channels_lab*2),device=cfg.device,dtype=torch.float32)
+        # if loss_type == 'bce':
+        #     batch_iou_bg = torch.zeros(size=(len(valid_loader.dataset.img_names),2),device=cfg.device,dtype=torch.float32)
         
         for input_var, target_var, batch_names_valid in valid_loader:
 
             model_output = segmentation_net.forward(input_var)
             val_loss = loss_calc(loss_type,criterion,model_output,target_var, cfg.num_channels_lab ,cfg.use_mask)
-            # print("############### VALIDATION #####################")
-            # print(torch.unique(input_var))
-            # print(model_output)
-            # print(target_var)
-            # print(model_output.shape)
-            # print(target_var.shape)
-            # print(len(model_output))
-            # print(len(target_var))
-            print("################ VALIDATION #####################")
+            print("Validation: ", end="")
             print(val_loss)
             cfg.validation_losses.append(val_loss.data)
-
-            index_end = index_start + len(batch_names_valid)
-            if loss_type == 'bce':
-                batch_iou[index_start:index_end, :], batch_iou_bg[index_start:index_end]= calc_metrics_pix(model_output, target_var,cfg.num_channels_lab,cfg.device,cfg.use_mask,loss_type)
-            elif loss_type == 'ce':
-                batch_iou[index_start:index_end, :]= calc_metrics_pix(model_output, target_var, cfg.num_channels_lab, cfg.device, cfg.use_mask,loss_type)
-            else:
-                print("Error: unimplemented loss type")
-                sys.exit(0)
-
-            index_start += len(batch_names_valid)
-
             cfg.count_val += 1
+            # index_end = index_start + len(batch_names_valid)
+            # if loss_type == 'bce':
+            #     batch_iou[index_start:index_end, :], batch_iou_bg[index_start:index_end]= calc_metrics_pix(model_output, target_var,cfg.num_channels_lab,cfg.device,cfg.use_mask,loss_type)
+            # elif loss_type == 'ce':
+            #     batch_iou[index_start:index_end, :]= calc_metrics_pix(model_output, target_var, cfg.num_channels_lab, cfg.device, cfg.use_mask,loss_type)
+            # else:
+            #     print("Error: unimplemented loss type")
+            #     sys.exit(0)
+
+            # index_start += len(batch_names_valid)
+
+            
 
         scheduler.step(torch.mean(torch.tensor(cfg.validation_losses)))
         ##############################################################
         ### Racunanje finalne metrike nad celim validacionim setom ###
         ##############################################################
 
-        if loss_type == 'bce':
-            final_metric_calculation(loss_type = loss_type, epoch=epoch,num_channels_lab = cfg.num_channels_lab,
-            classes_labels = cfg.classes_labels, batch_iou_bg = batch_iou_bg, batch_iou = batch_iou, train_part = train_part)
-        elif loss_type == 'ce':
-            final_metric_calculation(loss_type = loss_type, epoch = epoch, num_channels_lab = cfg.num_channels_lab, 
-            classes_labels = cfg.classes_labels, batch_iou = batch_iou, train_part = train_part)
-        else:
-            print("Error: Unimplemented loss type!")
-            sys.exit(0)
+        # if loss_type == 'bce':
+        #     final_metric_calculation(loss_type = loss_type, epoch=epoch,num_channels_lab = cfg.num_channels_lab,
+        #     classes_labels = cfg.classes_labels, batch_iou_bg = batch_iou_bg, batch_iou = batch_iou, train_part = train_part)
+        # elif loss_type == 'ce':
+        #     final_metric_calculation(loss_type = loss_type, epoch = epoch, num_channels_lab = cfg.num_channels_lab, 
+        #     classes_labels = cfg.classes_labels, batch_iou = batch_iou, train_part = train_part)
+        # else:
+        #     print("Error: Unimplemented loss type!")
+        #     sys.exit(0)
 
         epoch_list[epoch] = epoch
         all_validation_losses[epoch] = (torch.mean(torch.tensor(cfg.validation_losses,dtype = torch.float32)))
-
         end_of_epoch_print(epoch,all_validation_losses)
+
+    ######## TEST ############
+    # CREATING IMAGES TO SEE IF NETWORKS ARE OUTPUTTING JUNK OR NOT
+    i = 0
+    for input_var, target_var, batch_names_valid in test_loader:
+        i+=1
+        model_output = segmentation_net.forward(input_var)
+        sigmoid_function = torch.nn.Sigmoid()
+        model_output = sigmoid_function(model_output)
+        
+        #extracting single images from batches
+        for x in range(model_output.shape[0]):
+            
+            #only drawing pixels with value above 0.5
+            thresholded = model_output[x, :, :, :]>0.5
+            thresholded_tmp = thresholded.byte()
+            picturex = torch.squeeze(thresholded_tmp)
+            picturex = picturex.detach().numpy()
+            #converting to float so picture can be drawn in png format
+            picturex = picturex.astype(float)
+            im = Image.fromarray(picturex, 'L')
+            im.save("Slike/" +cfg.net_type + "/" + cfg.net_type + str(i) + str(x) + ".png")
+
+            
+            #drawing the target images for comparison
+            thresholded_target = target_var[x, :, :, :]>0.5
+            thresholded_target_tmp = thresholded_target.byte()
+            picturex_target = torch.squeeze(thresholded_target_tmp)
+            picturex_target = picturex_target.detach().numpy()
+            picturex_target = picturex_target.astype(float)
+            im_target = Image.fromarray(picturex_target, 'L')
+            im_target.save("Slike/" + cfg.net_type + "/Test_" + cfg.net_type + str(i) + str(x) + ".png")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='My program description')
     parser.add_argument('--learning_rate', type=float, default=0.001, metavar="N")
     parser.add_argument('--lambda_parametar', type=int, default=0.99, metavar="N")
     parser.add_argument('--stepovi_arr', type=int, default=5, metavar="N")
-    parser.add_argument('--num_epochs', type=int, default=3, metavar="N")
+    parser.add_argument('--num_epochs', type=int, default=1, metavar="N")
     parser.add_argument('--loss_type', type=str, default="bce", metavar="N")
     parser.add_argument('--Batch_size', type=int, default=8, metavar="N")
-    parser.add_argument('--net_type', type=str, default="SegNet", metavar="N")
-    parser.add_argument('--trening_location', default="trening_set_mini2/img", type=str)
-    parser.add_argument('--validation_location', default="validation_set_mini2/img", type=str)
-    parser.add_argument('--test_location', default="test_set_mini2/img", type=str)
+    parser.add_argument('--net_type', type=str, default="UNet3", metavar="N")
+    parser.add_argument('--trening_location', default="C:/Users/daniv/Downloads/FullSet/trening_set_mini2/img", type=str)
+    parser.add_argument('--validation_location', default="C:/Users/daniv/Downloads/FullSet/validation_set_mini2/img", type=str)
+    parser.add_argument('--test_location', default="C:/Users/daniv/Downloads/FullSet/test_set_mini2/img", type=str)
     parser.add_argument('--new_location', default="", type=str)
     args = parser.parse_args()
     config = config_func_unet3(False, args.net_type)
